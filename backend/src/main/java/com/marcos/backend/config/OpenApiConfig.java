64b02b04fe3f245bee2b5b180ff6ccd4c48de4db
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-02 17:14:13 -0300

    config: criado configuraçoes do Swagger

---
+++ backend/src/main/java/com/marcos/backend/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.marcos.backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+	@Bean
+	public OpenAPI apiInfo() {
+		return new OpenAPI().info(new Info().title("Teste Tecnico - OMS")
+				.version("1.0.0")
+				.description("API para teste de tecnico, gerenciando pedidos de produtos"));
+	}
+
+	@Bean
+	public GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder().group("public-api").pathsToMatch("/api/public/**").build();
+	}
+
+}
+
+
