59d3e86aaa313df4f3ec8080dca5bf6558cda45a
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-03 16:41:40 -0300

    fix: corrigido problemas do swagger

--- backend/src/main/java/com/marcos/backend/config/OpenApiConfig.java
+++ backend/src/main/java/com/marcos/backend/config/OpenApiConfig.java
@@ -17,7 +17,7 @@ public class OpenApiConfig {
 
 	@Bean
 	public GroupedOpenApi publicApi() {
-		return GroupedOpenApi.builder().group("public-api").pathsToMatch("/api/public/**").build();
+		return GroupedOpenApi.builder().group("public-api").pathsToMatch("/**").build();
 	}
 
 }
