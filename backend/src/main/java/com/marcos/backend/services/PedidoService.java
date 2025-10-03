2183210357475ac9170040b785e1661ab3fc9450
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-03 12:02:00 -0300

    feat: criado servico de pedido

---
+++ backend/src/main/java/com/marcos/backend/services/PedidoService.java
@@ -0,0 +1,48 @@
+package com.marcos.backend.services;
+
+import com.marcos.backend.entity.Pedido;
+import com.marcos.backend.entity.Produto;
+import com.marcos.backend.entity.StatusPedido;
+import com.marcos.backend.repository.PedidoRepository;
+import com.marcos.backend.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+	private final PedidoRepository pedidoRepository;
+	private final ProdutoRepository produtoRepository;
+
+	public PedidoService(PedidoRepository pedidoRepository, ProdutoRepository produtoRepository) {
+		this.pedidoRepository = pedidoRepository;
+		this.produtoRepository = produtoRepository;
+	}
+
+	@Transactional
+	public Pedido criarPedido(Pedido pedido) {
+		pedido.getItems().forEach(item -> {
+			Produto p = produtoRepository.findById(item.getProduto().getId())
+					.orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+			item.setProduto(p);
+		});
+		return pedidoRepository.save(pedido);
+	}
+
+	public List<Pedido> listPedidos() { return pedidoRepository.findAll(); }
+
+	public Optional<Pedido> getPedido(Integer id){ return pedidoRepository.findById(id); }
+
+	@Transactional
+	public void cancelaPedido(Integer id){
+		pedidoRepository.findById(id).ifPresent(o -> {
+			if (o.getStatus() != StatusPedido.Enviado && o.getStatus() != StatusPedido.Entregue) {
+				o.setStatus(StatusPedido.Cancelado);
+				pedidoRepository.save(o);
+			}
+		});
+	}
+
+}
