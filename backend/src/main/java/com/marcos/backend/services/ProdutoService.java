86e717dab7178144430c4772f32c17bc5df752c0
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-03 11:53:47 -0300

    feat: criado service de produto

---
+++ backend/src/main/java/com/marcos/backend/services/ProdutoService.java
@@ -0,0 +1,30 @@
+package com.marcos.backend.services;
+
+import com.marcos.backend.entity.Produto;
+import com.marcos.backend.repository.ProdutoRepository;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+	private final ProdutoRepository produtoRepository;
+	public ProdutoService(ProdutoRepository produtoRepository) {
+		this.produtoRepository = produtoRepository;
+	}
+
+	public Produto salvar(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	@Cacheable(value = "produtos")
+	public List<Produto> listAll(){ return produtoRepository.findAll(); }
+
+	@CacheEvict(value = "produtos", allEntries = true)
+	public void invalidateCache(){}
+
+	public Produto findById(Integer id){ return produtoRepository.findById(id).orElse(null); }
+
+}
