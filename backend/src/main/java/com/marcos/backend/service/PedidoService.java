ae2e95d42506954d4d61eb8e87ea6669883b2349
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-04 06:05:36 -0300

    ft: Altera status do pedido

--- backend/src/main/java/com/marcos/backend/service/PedidoService.java
+++ backend/src/main/java/com/marcos/backend/service/PedidoService.java
@@ -5,6 +5,7 @@ import com.marcos.backend.entity.Produto;
 import com.marcos.backend.entity.StatusPedido;
 import com.marcos.backend.repository.PedidoRepository;
 import com.marcos.backend.repository.ProdutoRepository;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -17,10 +18,12 @@ import java.util.Optional;
 public class PedidoService {
 	private final PedidoRepository pedidoRepository;
 	private final ProdutoRepository produtoRepository;
+	private final CacheManager cacheManager;
 
-	public PedidoService(PedidoRepository pedidoRepository, ProdutoRepository produtoRepository) {
+	public PedidoService(PedidoRepository pedidoRepository, ProdutoRepository produtoRepository, CacheManager cacheManager) {
 		this.pedidoRepository = pedidoRepository;
 		this.produtoRepository = produtoRepository;
+		this.cacheManager = cacheManager;
 	}
 
 	@Transactional
@@ -42,6 +45,7 @@ public class PedidoService {
 	public Optional<Pedido> getPedido(Integer id){ return pedidoRepository.findById(id); }
 
 	@Transactional
+	@CacheEvict(value = "pedidos", key = "#id")
 	public void cancelaPedido(Integer id){
 		pedidoRepository.findById(id).ifPresent(o -> {
 			if (o.getStatus() != StatusPedido.Enviado && o.getStatus() != StatusPedido.Entregue) {
@@ -49,6 +53,23 @@ public class PedidoService {
 				pedidoRepository.save(o);
 			}
 		});
+	}
+
+	@Transactional
+	@CacheEvict(value = "pedidos", key = "#id")
+	public Pedido alteraStatus(Integer id, StatusPedido status){
+		Pedido pedido = pedidoRepository.findById(id).orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
+		if(status.equals(StatusPedido.Pendente)) new RuntimeException("Pedido em Processamento, por favor aguarde");
+		pedido.setStatus(status);
+		if(status.equals(StatusPedido.Enviado)){
+			pedido.getItems().stream().forEach( p ->{
+				var produto = produtoRepository.findById(p.getProduto().getId()).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+				produto.setQuantidade(produto.getQuantidade() - p.getQuantidade());
+				produtoRepository.save(produto);
+				cacheManager.getCache("produtos").evict(produto.getId());
+			});
+		}
+		return pedidoRepository.save(pedido);
 	}
 
 }
