0aefc8ef8653e5bb09dbdf9ddcc17d80478ebc95
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-05 01:54:40 -0300

    ft: config roteamento

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { LoadingSpinner } from './shared/components/loading-spinner/loading-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, LoadingSpinner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
 }
