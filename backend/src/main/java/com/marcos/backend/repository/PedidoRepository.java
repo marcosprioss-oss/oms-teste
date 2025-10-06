346b84ca62384ec198aaab67ac7888d8d6e31854
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-06 10:16:36 -0300

    ft: paginação + consuta para estatisticas

--- backend/src/main/java/com/marcos/backend/repository/PedidoRepository.java
+++ backend/src/main/java/com/marcos/backend/repository/PedidoRepository.java
@@ -1,10 +1,12 @@
 package com.marcos.backend.repository;
 
 import com.marcos.backend.entity.Pedido;
+import com.marcos.backend.entity.Produto;
 import com.marcos.backend.entity.StatusPedido;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,4 +15,7 @@ public interface PedidoRepository extends JpaRepository<Pedido,Integer> {
 	List<Pedido> findByStatus(StatusPedido status);
 	Page<Pedido> findByStatus(Pedido status, Pageable pageable);
 	List<Pedido> findByCriacaoBetween(LocalDateTime from, LocalDateTime to);
+	@Query("SELECT p.status, COUNT(p) FROM Pedido p GROUP BY p.status")
+	List<Object[]> countPedidosPorStatus();
+	Page<Pedido> findAll(Pageable pageable);
 }
--- backend/src/main/java/com/marcos/backend/repository/ProdutoRepository.java
+++ backend/src/main/java/com/marcos/backend/repository/ProdutoRepository.java
@@ -1,6 +1,10 @@
 package com.marcos.backend.repository;
 
 import com.marcos.backend.entity.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {}
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	Page<Produto> findAll(Pageable pageable);
+}
