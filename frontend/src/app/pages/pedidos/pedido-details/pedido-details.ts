dbc5a08b41534fa5faedc0041dd1616f194dca25
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-06 09:57:00 -0300

    ft: detalhes do pedido

--- frontend/src/app/pages/pedidos/pedido-details/pedido-details.ts
+++ frontend/src/app/pages/pedidos/pedido-details/pedido-details.ts
@@ -2,25 +2,29 @@ import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { PedidoService } from '../../../core/services/pedido';
-import { Pedido, OrderStatus } from '../../../core/models/pedido.model';
+import { mapPedido, Pedido } from '../../../core/models/pedido.model';
 import { Loading } from '../../../core/services/loading';
 import { StatusBadge } from '../../../shared/components/status-badge/status-badge';
-
+import { StatusPedido } from '../../../core/models/status-pedido.enum';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-pedido-details',
-  imports: [CommonModule, RouterModule, StatusBadge],
+  imports: [CommonModule, RouterModule, StatusBadge, FormsModule, MatSnackBarModule],
   templateUrl: './pedido-details.html',
   styleUrl: './pedido-details.css'
 })
 export class PedidoDetails implements OnInit {
   pedido = signal<Pedido | null>(null);
   pedidoId = signal<number | null>(null);
+  protected readonly StatusPedido = StatusPedido;
 
   constructor(
     private pedidoService: PedidoService,
     private route: ActivatedRoute,
     private router: Router,
-    private loading: Loading
+    private loading: Loading,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit() {
@@ -37,7 +41,7 @@ export class PedidoDetails implements OnInit {
     this.loading.show();
     this.pedidoService.buscarPedidoPorId(id).subscribe({
       next: (pedido: Pedido) => {
-        this.pedido.set(pedido);
+        this.pedido.set(mapPedido(pedido));
         this.loading.hide();
       },
       error: (error) => {
@@ -64,11 +68,34 @@ export class PedidoDetails implements OnInit {
     }
   }
 
-  podeCancelar(status: OrderStatus): boolean {
-    return status !== OrderStatus.ENVIADO && status !== OrderStatus.ENTREGUE && status !== OrderStatus.CANCELADO;
+  podeCancelar(status: StatusPedido): boolean {
+    return status !== StatusPedido.ENVIADO && status !== StatusPedido.ENTREGUE && status !== StatusPedido.CANCELADO;
   }
 
   voltar() {
     this.router.navigate(['/pedidos']);
   }
+
+  podeEditarStatus(status: StatusPedido): boolean {
+    return (status !== StatusPedido.CANCELADO && status !== StatusPedido.PENDENTE);
+  }
+
+  alterarStatus(novoStatus: StatusPedido) {
+    if (!this.pedido()) return;
+
+    if (this.podeEditarStatus(this.pedido()!.status)) {
+      this.pedido()!.status = novoStatus;
+      this.pedidoService.alterarStatus(this.pedido()!.id!, novoStatus).subscribe({
+        next: () =>
+          this.snackBar.open('Status alterado com sucesso!', 'OK', {
+            duration: 3000,
+            verticalPosition: 'top',
+            horizontalPosition: 'end',
+          }),
+
+        error: (err) => console.error('Erro ao atualizar status', err)
+      });
+    }
+  }
+
 }
