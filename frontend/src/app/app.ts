5f473200b80e424050b9e3671724b170d2a7b62e
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-06 09:51:25 -0300

    ft: Estilizações gerais com Material

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { LoadingSpinner } from './shared/components/loading-spinner/loading-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, LoadingSpinner],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    LoadingSpinner
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
