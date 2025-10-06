3f4340676aab0609b5d4e66e0ecdfed36dc9b11d
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-10-06 09:54:21 -0300

    ft: spiner de loading

--- frontend/src/app/shared/components/loading-spinner/loading-spinner.ts
+++ frontend/src/app/shared/components/loading-spinner/loading-spinner.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Loading } from '../../../core/services/loading';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loading-spinner',
-  imports: [],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './loading-spinner.html',
   styleUrl: './loading-spinner.css'
 })
